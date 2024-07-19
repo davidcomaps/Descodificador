eab3cdc634f4f0fbb6d3bdb088f35bdfafb2c109
David Alexandre Campos <davidcomaps@gmail.com>
2024-07-19 16:13:50 -0300

    alterando a funcionalidade do input e output

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function criptografar() {
         .replace(/u/g, "ufat");
 
     mostrarResultado(textoCriptografado);
+    document.querySelector('#container__input__text').value = '';
 }
 
 function descriptografar() {
@@ -34,6 +35,7 @@ function descriptografar() {
         .replace(/ufat/g, "u");
 
     mostrarResultado(textoOriginal);
+    document.querySelector('#container__input__text').value = '';
 }
 
 function mostrarResultado(texto) {
@@ -56,8 +58,8 @@ function mostrarResultado(texto) {
 function copiar() {
     const result = document.querySelector('#container__output__control__result');
     navigator.clipboard.writeText(result.textContent).then(() => {
-        alert("Texto copiado para a área de transferência");
-
+        /* alert("Texto copiado para a área de transferência");
+ */
         document.querySelector('#container__input__text').value = '';
         result.textContent = '';
         result.classList.add('hidden');
